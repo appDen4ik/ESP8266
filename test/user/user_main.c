993ae6f12ff11a2d8f733eb01bd2289cadff3caa
rd3 <rd3@RD3.sbt.local>
2015-08-25 17:40:51 +0300

    update

--- test/user/user_main.c
+++ test/user/user_main.c
@@ -1,18 +1,40 @@
-
-
 #include <ets_sys.h>
 #include <osapi.h>
+#include <os_type.h>
 #include <gpio.h>
 
 // see eagle_soc.h for these definitions
-#define LED_GPIO 2
-#define LED_GPIO_MUX PERIPHS_IO_MUX_GPIO2_U
-#define LED_GPIO_FUNC FUNC_GPIO2
+#define OUT_1_GPIO 4
+#define OUT_1_MUX PERIPHS_IO_MUX_GPIO4_U
+#define OUT_1_FUNC FUNC_GPIO4
 
-#define DELAY 500000 /* microseconds */
+#define OUT_1_TIME 15
+#define OUT_2_TIME 35
 
-extern void ets_wdt_enable (void);
-extern void ets_wdt_disable (void);
+#define OUT_2_GPIO 5
+#define OUT_2_MUX PERIPHS_IO_MUX_GPIO5_U
+#define OUT_2_FUNC FUNC_GPIO5
+
+#define DELAY 10 /* milliseconds */
+
+LOCAL os_timer_t blink_timer;
+LOCAL uint8_t led_state1=0;
+LOCAL uint8_t led_state2=0;
+uint8_t out1 = 0;
+uint8_t out2 = 0;
+
+LOCAL void ICACHE_FLASH_ATTR blink_cb(void *arg)
+{
+	if (OUT_1_TIME == out1++) {
+		GPIO_OUTPUT_SET(OUT_1_GPIO, led_state1);
+		led_state1 ^=1;
+		out1 = 0;
+	} else if (OUT_2_TIME == out2++) {
+		GPIO_OUTPUT_SET(OUT_2_GPIO, led_state2);
+		led_state2 ^=1;
+		out2 = 0;
+	}
+}
 
 void user_rf_pre_init(void)
 {
@@ -20,15 +42,14 @@ void user_rf_pre_init(void)
 
 void user_init(void)
 {
-	uint8_t state=0;
-	ets_wdt_enable();
-	ets_wdt_disable();
 	// Configure pin as a GPIO
-	PIN_FUNC_SELECT(LED_GPIO_MUX, LED_GPIO_FUNC);
-	for(;;)
-	{
-		GPIO_OUTPUT_SET(LED_GPIO, state);
-		os_delay_us(DELAY);
-		state ^=1;
-	}
+	PIN_FUNC_SELECT(OUT_1_MUX, OUT_1_FUNC);
+	PIN_FUNC_SELECT(OUT_2_MUX, OUT_2_FUNC);
+	// Set up a timer to blink the LED
+	// os_timer_disarm(ETSTimer *ptimer)
+	os_timer_disarm(&blink_timer);
+	// os_timer_setfn(ETSTimer *ptimer, ETSTimerFunc *pfunction, void *parg)
+	os_timer_setfn(&blink_timer, (os_timer_func_t *)blink_cb, (void *)0);
+	// void os_timer_arm(ETSTimer *ptimer,uint32_t milliseconds, bool repeat_flag)
+	os_timer_arm(&blink_timer, DELAY, 1);
 }
