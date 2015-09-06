511d0b82102400e27b31266867a2a9407cdcb022
Den4ik <Den4ik@Den4ik-PC>
2015-09-07 00:50:20 +0300

    update

--- flashWrite/user/user_main.c
+++ flashWrite/user/user_main.c
@@ -11,9 +11,7 @@
 
 uint8 data[] = "Hello World";
 
-uint8 hello[6];
-uint8 world[6];
-
+uint8 hello[0x1000] = "1111111111";
 
 
 void user_rf_pre_init(void)
@@ -25,31 +23,55 @@ void user_rf_pre_init(void)
 //Init function 
 void ICACHE_FLASH_ATTR
 user_init() {
+	uint32_t i;
   extern void ets_wdt_disable(void);
 
 
   uart_init(BIT_RATE_115200, BIT_RATE_115200);
 
-  world[5] = '/0';
-  hello[5] = '\0';
 
-  if ( spi_flash_write( 70000, (uint32 *)data, strlen(data) ) != 0 ){
+//  spi_flash_erase_sector(0x12);
+/*  if ( spi_flash_write( 0x12000, (uint32 *)data, 10 ) != 0 ){
 
 	  ets_uart_printf("Error");
 
   } else {
 	  ets_uart_printf("Write done");
-	  spi_flash_read( 70000, (uint32 *)hello, 5 );
-	  spi_flash_read( 70005, (uint32 *)world, 5 );
-//	  ets_uart_printf(hello);
+	  if ( spi_flash_read( 0x12000, (uint32 *)hello, 10 ) != 0 ){
+
+	 	  ets_uart_printf("Error");
+
+	   } else {
+		   ets_uart_printf("Read done");
 	  uart_tx_one_char(hello[0]);
 	  uart_tx_one_char(hello[1]);
 	  uart_tx_one_char(hello[2]);
 	  uart_tx_one_char(hello[3]);
 	  uart_tx_one_char(hello[4]);
+	  uart_tx_one_char(hello[5]);
+	  uart_tx_one_char(hello[6]);
+	  uart_tx_one_char(hello[7]);
+	  uart_tx_one_char(hello[8]);
+	  uart_tx_one_char(hello[9]);
 	  ets_uart_printf("done");
-//	  ets_uart_printf(world);
+	   }
 
   }
 
+  */
+
+//  spi_flash_write( 0x12000, (uint32 *)data, sizeof(data) );
+  spi_flash_read( 0x12000, (uint32 *)hello, 0x1000 );
+  for (  i = 0; i < 0x1000; i++ ){
+	  uart_tx_one_char(hello[i]);
+  }
+  spi_flash_read( 0x13000, (uint32 *)hello, 0x1000 );
+  for (  i = 0; i < 0x1000; i++ ){
+	  uart_tx_one_char(hello[i]);
+  }
 }
+
+
+
+
+
