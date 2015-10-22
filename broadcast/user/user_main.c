21b1d6cc4e6585eb7c39db05e911de470c292fda
Den4ik <Den4ik@Den4ik-PC>
2015-10-23 01:28:05 +0300

    some tests

--- broadcast/user/user_main.c
+++ broadcast/user/user_main.c
@@ -22,6 +22,67 @@ LOCAL esp_udp udp;
 #define DELAY 100 /* milliseconds */
 
 
+void setup_wifi_ap_mode(void)
+{
+//	wifi_set_opmode((wifi_get_opmode()|STATIONAP_MODE)&USE_WIFI_MODE);
+	struct softap_config apconfig;
+	if(wifi_softap_get_config(&apconfig))
+	{
+		wifi_softap_dhcps_stop();
+		os_memset(apconfig.ssid, 0, sizeof(apconfig.ssid));
+		os_memset(apconfig.password, 0, sizeof(apconfig.password));
+//		apconfig.ssid_len = os_sprintf(apconfig.ssid, WIFI_AP_NAME);
+//		os_sprintf(apconfig.password, "%s", WIFI_AP_PASSWORD);
+		apconfig.authmode = AUTH_WPA_WPA2_PSK;
+		apconfig.ssid_hidden = 0;
+		apconfig.channel = 7;
+		apconfig.max_connection = 4;
+		if(!wifi_softap_set_config(&apconfig))
+		{
+			#ifdef PLATFORM_DEBUG
+			ets_uart_printf("ESP8266 not set AP config!\r\n");
+			#endif
+		};
+		struct ip_info ipinfo;
+		wifi_get_ip_info(SOFTAP_IF, &ipinfo);
+		IP4_ADDR(&ipinfo.ip, 192, 168, 4, 1);
+		IP4_ADDR(&ipinfo.gw, 192, 168, 4, 1);
+		IP4_ADDR(&ipinfo.netmask, 255, 255, 255, 0);
+		wifi_set_ip_info(SOFTAP_IF, &ipinfo);
+		wifi_softap_dhcps_start();
+	}
+	#ifdef PLATFORM_DEBUG
+	ets_uart_printf("ESP8266 in AP mode configured.\r\n");
+	#endif
+}
+
+void setup_wifi_st_mode(void)
+{
+//	wifi_set_opmode((wifi_get_opmode()|STATIONAP_MODE)&USE_WIFI_MODE);
+	struct station_config stconfig;
+	wifi_station_disconnect();
+	wifi_station_dhcpc_stop();
+	if(wifi_station_get_config(&stconfig))
+	{
+		os_memset(stconfig.ssid, 0, sizeof(stconfig.ssid));
+		os_memset(stconfig.password, 0, sizeof(stconfig.password));
+		os_sprintf(stconfig.ssid, "%s", "Default STA");
+		os_sprintf(stconfig.password, "%s", "76543210");
+		if(!wifi_station_set_config(&stconfig))
+		{
+			#ifdef PLATFORM_DEBUG
+			ets_uart_printf("ESP8266 not set station config!\r\n");
+			#endif
+		}
+	}
+	wifi_station_connect();
+	wifi_station_dhcpc_start();
+	wifi_station_set_auto_connect(1);
+	#ifdef PLATFORM_DEBUG
+	ets_uart_printf("ESP8266 in STA mode configured.\r\n");
+	#endif
+}
+
 
 LOCAL os_timer_t blink_timer;
 
@@ -54,10 +115,18 @@ user_init(void)
 	char ssid[33] = "TEST";
 	char password[33] = "123456789";
 
+
+
+
+
+//	system_restore();
+
 	ets_wdt_enable();
 	ets_wdt_disable();
 
-	wifi_set_opmode( SOFTAP_MODE );
+	wifi_set_opmode( STATIONAP_MODE );
+
+
 
 	wifi_softap_dhcps_stop();
 
@@ -81,24 +150,58 @@ user_init(void)
 	apConfig.max_connection = 255;
 	apConfig.ssid_hidden = 0;
 
+
 	wifi_softap_set_config(&apConfig);
 
 
 
+	struct station_config stationConf;
+
+	wifi_station_disconnect();
+	wifi_station_dhcpc_stop();
+	wifi_station_get_config(&stationConf);
+
+	os_memset(stationConf.ssid, 0, sizeof(stationConf.ssid));
+	os_memset(stationConf.password, 0, sizeof(stationConf.password));
+	os_sprintf(stationConf.ssid,     "%s" ,"Default STA" );
+	os_sprintf(stationConf.password, "%s", "76543210" );
+//	os_sprintf(stationConf.ssid, "%s", "Default STA");
+//	os_sprintf(stationConf.password, "%s", "76543210");
+	wifi_station_set_config(&stationConf);
+
+	wifi_station_connect();
+	wifi_station_dhcpc_start();
+	wifi_station_set_auto_connect(1);
 
 
-	PIN_FUNC_SELECT(LED_GPIO_MUX, LED_GPIO_FUNC);
+//	struct station_config stconfig;
+
+/*	wifi_station_disconnect();
+	wifi_station_dhcpc_stop();
+	wifi_station_get_config(&stationConf);
+
+		os_memset(stationConf.ssid, 0, sizeof(stationConf.ssid));
+		os_memset(stationConf.password, 0, sizeof(stationConf.password));
+		os_sprintf(stationConf.ssid, "%s", "Default STA");
+		os_sprintf(stationConf.password, "%s", "76543210");
+		wifi_station_set_config(&stationConf);
+
+	wifi_station_connect();
+	wifi_station_dhcpc_start();
+	wifi_station_set_auto_connect(1);
+
+//	PIN_FUNC_SELECT(LED_GPIO_MUX, LED_GPIO_FUNC);
 
 
 
 	// Set up a timer to blink the LED
 	// os_timer_disarm(ETSTimer *ptimer)
-	os_timer_disarm(&blink_timer);
+//	os_timer_disarm(&blink_timer);
 	// os_timer_setfn(ETSTimer *ptimer, ETSTimerFunc *pfunction, void *parg)
-	os_timer_setfn(&blink_timer, (os_timer_func_t *)sendDatagram, (void *)0);
+//	os_timer_setfn(&blink_timer, (os_timer_func_t *)sendDatagram, (void *)0);
 	// void os_timer_arm(ETSTimer *ptimer,uint32_t milliseconds, bool repeat_flag)
-	os_timer_arm(&blink_timer, DELAY, 1);
+//	os_timer_arm(&blink_timer, DELAY, 1);
 
 
-
+*/
 }
