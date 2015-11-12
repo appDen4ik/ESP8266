d8b1d265eea6edf020bf33b592598d1f0d1674b0
rd3 <rd3@RD3.sbt.local>
2015-11-12 17:13:04 +0200

    update

--- testNetwork/include/user_config.h
+++ testNetwork/include/user_config.h
@@ -2,8 +2,8 @@
 #define __USER_CONFIG_H__
 
 #define USE_WIFI_MODE		STATION_MODE
-#define WIFI_CLIENTSSID		/*"SomeWorkNetwork"*//*"DIR-320"*/"TSM_Guest"
-#define WIFI_CLIENTPASSWORD	/*"76543210"/*"76543210"*/"tsmguest"
+#define WIFI_CLIENTSSID		/*"a"*//*"SomeWorkNetwork"*//*"DIR-320"*/"TSM_Guest"
+#define WIFI_CLIENTPASSWORD	/*"Default STA"*//*"76543210"/*"76543210"*/"tsmguest"
 #define WIFI_AP_NAME		"ESP8266"
 #define WIFI_AP_PASSWORD	"00000000"
 #define TCPSERVERIP		"192.168.1.100"
--- testNetwork/user/user_main.c
+++ testNetwork/user/user_main.c
@@ -5,35 +5,42 @@
 #include "driver/uart.h"
 #include "user_config.h"
 #include "espconn.h"
-
+#define WIFI_CLIENTSSID		"TSM_Guest"
+#define WIFI_CLIENTPASSWORD	"tsmguest"
 
 extern int ets_uart_printf(const char *fmt, ...);
 int (*console_printf)(const char *fmt, ...) = ets_uart_printf;
-static ETSTimer WiFiLinker;
 struct espconn espconnServer;
 esp_tcp tcpServer;
 extern void uart_tx_one_char();
 
-uint8_t writeFlashTmp[4096];
+LOCAL struct  espconn espconnBroadcastSTA;
+LOCAL esp_udp espudpBroadcastSTA;
+LOCAL uint8_t brodcastMessage[] = "Hello World!\r\n";
 
+uint8_t data[] = "GET / HTTP/1.1\r\nHost: gcc.gnu.org\r\n\r\n";
 LOCAL struct espconn espconnClientTCP;
 LOCAL esp_tcp clientEsp_TCP;
 
+LOCAL struct  espconn espconnBroadcastAP;
+LOCAL esp_udp espudpBroadcastAP;
 
-static void wifi_check_ip(void *arg);
 LOCAL os_timer_t task_timer;
 
-void tcp_disnconcb( void *arg ) {
+static void ICACHE_FLASH_ATTR
+tcp_disnconcb( void *arg ) {
 
 	ets_uart_printf (" |tcp_disnconcb| ");
 }
 
-void tcp_sentcb( void *arg ) {
+static void ICACHE_FLASH_ATTR
+tcp_sentcb( void *arg ) {
 
 	 ets_uart_printf (" |tcp_sentcb| ");
 }
 
-void tcp_reconcb( void *arg, sint8 err ) {
+static void ICACHE_FLASH_ATTR
+tcp_reconcb( void *arg, sint8 err ) {
 
 	 ets_uart_printf (" |tcp_reconcb| ");
 }
@@ -48,36 +55,55 @@ tcp_recvcb( void *arg, char *pdata, unsigned short len ) { // data received
     }
 }
 
- void ICACHE_FLASH_ATTR tcp_connectcb(void *arg) {
+ void ICACHE_FLASH_ATTR
+ tcp_connectcb(void *arg) {
 
 	 ets_uart_printf (" |tcp_connectcb| ");
 }
 
+ static void ICACHE_FLASH_ATTR
+ tcpclient_sent_cb( void *arg ) {
 
- static void ICACHE_FLASH_ATTR tcpclient_recon_cb(void *arg, sint8 err)
- {
+ 	 ets_uart_printf (" tcpclient_sent_cb ");
+ }
+
+ static void ICACHE_FLASH_ATTR
+ tcpclient_recon_cb(void *arg, sint8 err) {
+
  	ets_uart_printf("tcpclient_recon_cb\r\n");
  	 uart_tx_one_char(err);
  }
 
- void ICACHE_FLASH_ATTR tcpclient_connect_cb(void *arg){
+ static void ICACHE_FLASH_ATTR
+ tcpclient_connect_cb(void *arg) {
  	ets_uart_printf("tcpclient_connect_cb\r\n");
- 	espconn_disconnect((struct espconn *)arg);
+ 	os_printf("printf %s", data);
+ 	espconn_send( arg, data, sizeof(data) );
+//espconn_disconnect((struct espconn *)arg);
  }
 
- static void ICACHE_FLASH_ATTR tcpclient_discon_cb(void *arg)
- {
+ static void ICACHE_FLASH_ATTR
+ tcpclient_recv_cb ( void *arg, char *pdata, unsigned short len ) {
+
+	 uint16 i;
+	  for (i = 0; i < len; i++) {
+	     uart_tx_one_char(pdata[i]);
+	 }
+ }
+
+ static void ICACHE_FLASH_ATTR
+ tcpclient_discon_cb(void *arg) {
+
  	ets_uart_printf("tcpclient_discon_cb\r\n");
  }
 
- static void ICACHE_FLASH_ATTR senddata( void )
- {
+ static void ICACHE_FLASH_ATTR senddata( void ) {
  	char info[150];
  	char tcpserverip[15];
  	espconnClientTCP.proto.tcp = &clientEsp_TCP;
  	espconnClientTCP.type = ESPCONN_TCP;
  	espconnClientTCP.state = ESPCONN_NONE;
- 	os_sprintf(tcpserverip, "%s", "212.42.83.53");
+ 	os_sprintf(tcpserverip, "%s", "209.132.180.131");
  	uint32_t ip = ipaddr_addr(tcpserverip);
  	os_memcpy(espconnClientTCP.proto.tcp->remote_ip, &ip, 4);
  	espconnClientTCP.proto.tcp->local_port = espconn_port();
@@ -85,15 +111,11 @@ tcp_recvcb( void *arg, char *pdata, unsigned short len ) { // data received
  	espconn_regist_connectcb(&espconnClientTCP, tcpclient_connect_cb);
  	espconn_regist_reconcb(&espconnClientTCP, tcpclient_recon_cb);
  	espconn_regist_disconcb(&espconnClientTCP, tcpclient_discon_cb);
- /*	#ifdef PLATFORM_DEBUG
- 	console_printf("Start espconn_connect to " IPSTR ":%d\r\n", IP2STR(espconnClientTCP.proto.tcp->remote_ip), espconnClientTCP.proto.tcp->remote_port);
- 	#endif
- 	#ifdef LWIP_DEBUG
- 	console_printf("LWIP_DEBUG: Start espconn_connect local port %u\r\n", Conn.proto.tcp->local_port);
- 	#endif*/
+ 	espconn_regist_recvcb(&espconnClientTCP, tcpclient_recv_cb);
+ 	espconn_regist_sentcb(&espconnServer, tcpclient_sent_cb);
  	sint8 espcon_status = espconn_connect(&espconnClientTCP);
- /*	#ifdef PLATFORM_DEBUG
- 	switch(espcon_status)
+ #ifdef PLATFORM_DEBUG
+ 	switch( espcon_status )
  	{
  		case ESPCONN_OK:
  			console_printf("TCP created.\r\n");
@@ -107,31 +129,51 @@ tcp_recvcb( void *arg, char *pdata, unsigned short len ) { // data received
  		default:
  			console_printf("Connection error: %d\r\n", espcon_status);
  	}
- 	#endif*/
-
+ #endif
  }
-
 
  LOCAL void ICACHE_FLASH_ATTR
  mScheduler(void) {
 
  	os_timer_disarm(&task_timer);
+ 	struct station_info *station = wifi_softap_get_station_info();
+ 	LOCAL struct ip_info inf;
 
- 	//wifi_station_dhcpc_stop();
- 	//wifi_station_dhcpc_start();
+	while ( station ) {
 
- 		switch( wifi_station_get_connect_status() ) {
+#ifdef DEBUG
+	os_printf( "bssid : %x:%x:%x:%x:%x:%x ip : %d.%d.%d.%d ", MAC2STR( station->bssid ), IP2STR( &station->ip ) );
+#endif
+		espconnBroadcastAP.type = ESPCONN_UDP;
+		espconnBroadcastAP.state = ESPCONN_NONE;
+		espconnBroadcastAP.proto.udp = &espudpBroadcastAP;
+	    espconnBroadcastAP.proto.udp->remote_port =  9876 ;
+	    IP4_ADDR( (ip_addr_t *)espconnBroadcastAP.proto.udp->remote_ip, (uint8_t)(station->ip.addr), \
+	    		(uint8_t)(station->ip.addr >> 8),\
+				(uint8_t)(station->ip.addr >> 16), (uint8_t)(station->ip.addr >> 24) );
+		espconn_create( &espconnBroadcastAP );
+		espconn_send( &espconnBroadcastAP, brodcastMessage, strlen( brodcastMessage ) );
+		espconn_delete(&espconnBroadcastAP);
+
+		station = STAILQ_NEXT(station, next);
+	}
+	wifi_softap_free_station_info();
+
+ 	switch( wifi_station_get_connect_status() ) {
  			case STATION_GOT_IP:
  				ets_uart_printf( "WiFi connected\r\n " );
- 				espconn_delete(&espconnServer);
- 				espconn_regist_recvcb(&espconnServer, tcp_recvcb);          // data received
- 				espconn_regist_connectcb(&espconnServer, tcp_connectcb);    // TCP connected successfully
- 				espconn_regist_disconcb(&espconnServer, tcp_disnconcb);     // TCP disconnected successfully
- 				espconn_regist_sentcb(&espconnServer, tcp_sentcb);          // data sent
- 				espconn_regist_reconcb(&espconnServer, tcp_reconcb);        // error, or TCP disconnected
- 				espconn_accept(&espconnServer);
- 				espconn_regist_time(&espconnServer, 20, 0);
- 				//senddata();
+ 				wifi_get_ip_info( STATION_IF, &inf );
+ 				os_printf( "ip %u : %d.%d.%d.%d ", (uint32)(inf.ip.addr), IP2STR( &(inf.ip.addr) ) );
+ 				espconnBroadcastSTA.type = ESPCONN_UDP;
+ 				espconnBroadcastSTA.state = ESPCONN_NONE;
+ 				espconnBroadcastSTA.proto.udp = &espudpBroadcastSTA;
+ 				espconnBroadcastSTA.proto.udp->remote_port =  9876;
+ 				IP4_ADDR((ip_addr_t *)espconnBroadcastSTA.proto.udp->remote_ip, (uint8_t)(inf.ip.addr), \
+ 						(uint8_t)(inf.ip.addr >> 8),
+                         (uint8_t)(inf.ip.addr >> 16), 255);
+ 				espconn_create( &espconnBroadcastSTA );
+ 				espconn_send( &espconnBroadcastSTA, brodcastMessage, strlen( brodcastMessage ) );
+ 				senddata();
  				break;
  			case STATION_WRONG_PASSWORD:
  				ets_uart_printf( "WiFi connecting error, wrong password\r\n" );
@@ -146,24 +188,22 @@ tcp_recvcb( void *arg, char *pdata, unsigned short len ) { // data received
  			default:
  				ets_uart_printf( "WiFi connecting...\r\n " );
  				break;
- 		}
+ 	}
 
-
+ 	espconn_delete( &espconnBroadcastSTA );
  	os_timer_setfn( &task_timer, (os_timer_func_t *)mScheduler, (void *)0 );
- 	os_timer_arm( &task_timer, 300, 0 );
+ 	os_timer_arm( &task_timer, 3000, 0 );
  }
 
+ static void ICACHE_FLASH_ATTR
+ user_rf_pre_init(void){
 
-
-void user_rf_pre_init(void)
-{
 }
 
-//Init function
-void ICACHE_FLASH_ATTR user_init()
-{
+ void ICACHE_FLASH_ATTR
+ user_init() {
 
-/*	struct softap_config apConfig;
+	struct softap_config apConfig;
 	struct ip_info ipinfo;
 	struct station_config stconfig;
 
@@ -173,10 +213,8 @@ void ICACHE_FLASH_ATTR user_init()
 
 	wifi_set_opmode(STATIONAP_MODE);
 
-	os_install_putc1(uart_tx_one_char);
-
 	uart_init(BIT_RATE_115200, BIT_RATE_115200);
-	os_delay_us(1000);
+	os_install_putc1(uart_tx_one_char);
 
 	ets_uart_printf("ESP8266 platform starting...\r\n");
 
@@ -189,11 +227,9 @@ void ICACHE_FLASH_ATTR user_init()
 		os_memset(stconfig.password, 0, sizeof(stconfig.password));
 		os_sprintf(stconfig.ssid, "%s", WIFI_CLIENTSSID);
 		os_sprintf(stconfig.password, "%s", WIFI_CLIENTPASSWORD);
-		if(!wifi_station_set_config(&stconfig))
-		{
-			#ifdef PLATFORM_DEBUG
+		if(!wifi_station_set_config(&stconfig)) {
+
 			ets_uart_printf("ESP8266 not set station config!\r\n");
-			#endif
 		}
 	}
 	wifi_station_connect();
@@ -207,8 +243,6 @@ void ICACHE_FLASH_ATTR user_init()
 			stationConfig.password);
 	}
 
-
-
 	wifi_softap_dhcps_stop();
 	os_sprintf(apConfig.ssid, "%s", "test");
 	os_sprintf(apConfig.password, "%s", "76543210");
@@ -219,7 +253,7 @@ void ICACHE_FLASH_ATTR user_init()
 	apConfig.ssid_hidden = 0;
 
 	wifi_get_ip_info(SOFTAP_IF, &ipinfo );
-	ipinfo.ip.addr = ipaddr_addr("172.18.4.1");
+	ipinfo.ip.addr = ipaddr_addr("93.158.134.3");
 	//ipinfo.gw.addr = ipinfo.ip.addr;  //шлюз
 	IP4_ADDR( &ipinfo.netmask, 255, 255, 255, 0 );
 	wifi_set_ip_info( SOFTAP_IF, &ipinfo );
@@ -233,7 +267,6 @@ void ICACHE_FLASH_ATTR user_init()
 	espconnServer.proto.tcp = &tcpServer;
 	espconnServer.proto.tcp->local_port = 6766;
 
-	//espconn_regist_time(&espconnServer, 60, 0);
 	espconn_regist_recvcb(&espconnServer, tcp_recvcb);          // data received
 	espconn_regist_connectcb(&espconnServer, tcp_connectcb);    // TCP connected successfully
 	espconn_regist_disconcb(&espconnServer, tcp_disnconcb);     // TCP disconnected successfully
@@ -241,60 +274,9 @@ void ICACHE_FLASH_ATTR user_init()
 	espconn_regist_reconcb(&espconnServer, tcp_reconcb);        // error, or TCP disconnected
 	espconn_accept(&espconnServer);
 	espconn_regist_time(&espconnServer, 20, 0);
-	// Wait for Wi-Fi connection and start TCP connection
 
-
-	// os_timer_disarm(ETSTimer *ptimer)
 	os_timer_disarm(&task_timer);
-	// os_timer_setfn(ETSTimer *ptimer, ETSTimerFunc *pfunction, void *parg)
 	os_timer_setfn(&task_timer, (os_timer_func_t *)mScheduler, (void *)0);
-	 //void os_timer_arm(ETSTimer *ptimer,uint32_t milliseconds, bool repeat_flag)
-	os_timer_arm(&task_timer, 200, 0);*/
-
-	uart_init(BIT_RATE_115200, BIT_RATE_115200);
-	os_install_putc1(uart_tx_one_char);
-	os_printf( " sdk version: %s \r\n", system_get_sdk_version() );
-	os_printf(" spi_flash_erase_sector( 63 ) %s ", spi_flash_erase_sector( 63 ) ? "false" : "true" );
-	os_printf(" spi_flash_erase_sector( 62 ) %s ", spi_flash_erase_sector( 62 ) ? "false" : "true" );
-	os_printf(" spi_flash_erase_sector( 61 ) %s ", spi_flash_erase_sector( 61 ) ? "false" : "true" );
-	os_printf(" spi_flash_erase_sector( 60 ) %s ", spi_flash_erase_sector( 60 ) ? "false" : "true" );
-
-	{
-
-	uint16_t c, currentSector;
-
-	for ( currentSector = 60; currentSector <= 63; currentSector++ ) {
-			os_printf( " currentSector %d\r\n", currentSector);
-			spi_flash_read( SPI_FLASH_SEC_SIZE * currentSector, (uint32 *)writeFlashTmp, SPI_FLASH_SEC_SIZE );
-			for ( c = 0; SPI_FLASH_SEC_SIZE > c; c++ ) {
-
-				uart_tx_one_char(writeFlashTmp[c]);
-			}
-
-			system_soft_wdt_stop();
-		}
-	}
-	os_printf(" spi_flash_erase_sector( 125 ) %s ", spi_flash_erase_sector( 125 ) ? "false" : "true" );
-	os_printf(" spi_flash_erase_sector( 124 ) %s ", spi_flash_erase_sector( 124 ) ? "false" : "true" );
-	os_printf(" spi_flash_erase_sector( 123 ) %s ", spi_flash_erase_sector( 123 ) ? "false" : "true" );
-	os_printf(" spi_flash_erase_sector( 122 ) %s ", spi_flash_erase_sector( 122 ) ? "false" : "true" );
-
-	{
-
-	uint16_t c, currentSector;
-
-	for ( currentSector = 122; currentSector <= 125; currentSector++ ) {
-			os_printf( " currentSector %d\r\n", currentSector);
-			spi_flash_read( SPI_FLASH_SEC_SIZE * currentSector, (uint32 *)writeFlashTmp, SPI_FLASH_SEC_SIZE );
-			for ( c = 0; SPI_FLASH_SEC_SIZE > c; c++ ) {
-
-				uart_tx_one_char(writeFlashTmp[c]);
-			}
-
-			system_soft_wdt_stop();
-		}
-	}
-	system_restore();
-	system_restart();
+	os_timer_arm(&task_timer, 1000, 0);
 }
 
